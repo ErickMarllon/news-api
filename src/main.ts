84176789bb5ca9cdb89614338d33d3736647acbb
erick.carvalho <erick.carvalho@teamsoft.com.br>
2024-07-14 01:16:00 -0300

    chore(main): configurar prefixo global e versionamento URI
    - Configura o prefixo global da aplicação baseado na variável de ambiente URI_PREFIX.
    - Habilita o versionamento URI com a versão padrão '1'.
    - Integra o Swagger para documentação da API com o prefixo e porta configurados.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { setupSwagger } from './main/config/swagger';
+import * as dotenv from 'dotenv';
+import { VersioningType } from '@nestjs/common';
 async function bootstrap() {
+  dotenv.config();
+  const prefix = `${process.env.URI_PREFIX}`;
+  const port = process.env.PORT;
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.setGlobalPrefix(prefix);
+  app.enableVersioning({ type: VersioningType.URI, defaultVersion: ['1'] });
+
+  setupSwagger(prefix, port, app);
+
+  await app.listen(port);
 }
 bootstrap();
