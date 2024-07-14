2312b168178d9d1b15e9d13ede184b5d65644095
erick.carvalho <erick.carvalho@teamsoft.com.br>
2024-07-14 01:18:00 -0300

    feat(app): adicionar EnvironmentConfigModule ao AppModule
    - Importa o EnvironmentConfigModule para fornecer configurações de ambiente à aplicação.
    - Injeta o AppController como controlador principal e AppService como provedor de serviços.
    
    # Este commit adiciona o módulo de configuração de ambiente à estrutura do aplicativo,
    # garantindo que as configurações ambientais sejam gerenciadas de forma centralizada.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EnvironmentConfigModule } from './main/config/environment-config';
 
 @Module({
-  imports: [],
+  imports: [EnvironmentConfigModule],
   controllers: [AppController],
   providers: [AppService],
 })
